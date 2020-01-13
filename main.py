d3342ef4e6fa5387098258726354830e4122eb02
zarramarra <47611649+zarramarra@users.noreply.github.com>
2020-01-13 11:14:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+dataframe = pd.read_csv("titanic.csv", delimiter=",")
+dataframe = dataframe.replace({"Survived": {0: "Did Not Survive", 1: "Survived"}})
+sns.scatterplot(x="Age", y="Fare", hue="Survived", data=dataframe)
+
+plt.xlabel("Passenger Age")
+plt.ylabel("Passenger Fare")
+plt.savefig("titanic.png")
